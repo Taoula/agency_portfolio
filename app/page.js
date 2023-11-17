433537903e0ec9f59d2a3ee48d965457af2a02ac
Ryan McCord <mccordryan00@gmail.com>
2023-11-17 15:42:26 -0600

    debounced scroll listener to increment step

--- app/page.js
+++ app/page.js
@@ -1,18 +1,47 @@
 "use client"
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import debounce from "lodash.debounce"
 
 export default function Home() {
 
   const [step, setStep] = useState(0);
+
+  const canIncrement = useRef(true);
 
   const landingRef = useRef(null);
   const aboutRef = useRef(null);
   const portfolioRef = useRef(null);
   const contactRef = useRef(null);
 
+  useEffect(() => {
+    const resetIncrement = debounce(() => {
+      canIncrement.current = true;
+    }, 2000);
+
+    const handleScroll = (event) => {
+      if (canIncrement.current) {
+        setStep(prevStep => prevStep + 1);
+        canIncrement.current = false;
+        resetIncrement();
+      }
+    };
+
+    window.addEventListener('wheel', handleScroll);
+
+    return () => {
+      window.removeEventListener('wheel', handleScroll);
+      resetIncrement.cancel();
+    };
+  }, []);
+
+  useEffect(() => {
+    //scrollTo new step when it changes.
+  }, [step])
+
   return (
     <>
+      <p>{step}</p>
       <div ref={landingRef}>
 
       </div>
