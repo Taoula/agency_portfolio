64362c3b4faa097fc927dd2427e071b46e13090d
Ryan McCord <mccordryan00@gmail.com>
2023-11-17 19:56:32 -0600

    idk

--- app/page.js
+++ app/page.js
@@ -88,7 +88,7 @@ export default function Home() {
             </div>
           </div>
           <div ref={portfolioRef}>
-            <div className="h-screen w-screen bg-orange-100 relative">
+            <div className="h-screen w-screen bg-orange-100 relative -z-10 text-slate-600">
               <div className="absolute top-60 left-56">
                 <FramerShell direction="right">
                   <div className="flex items-center gap-2 hover:scale-110 duration-200">
@@ -100,8 +100,9 @@ export default function Home() {
               <div className="absolute top-60 right-56">
                 <FramerShell direction="left">
                   <div className="flex items-center gap-2 hover:scale-110 duration-200">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="64" viewBox="0 0 32 32" width="64"><path d="M19.62 11.558l-3.203 2.98-2.972-5.995 1.538-3.448c.4-.7 1.024-.692 1.414 0z" fill="#ffa000" /><path d="M13.445 8.543l2.972 5.995-11.97 11.135z" fill="#f57f17" /><path d="M23.123 7.003c.572-.55 1.164-.362 1.315.417l3.116 18.105-10.328 6.2c-.36.2-1.32.286-1.32.286s-.874-.104-1.207-.3L4.447 25.673z" fill="#ffca28" /><path d="M13.445 8.543l-8.997 17.13L8.455.638c.148-.78.592-.855.988-.167z" fill="#ffa000" /></svg>
                     <h1 className="text-3xl font-semibold">Firebase</h1>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="64" viewBox="0 0 32 32" width="64"><path d="M19.62 11.558l-3.203 2.98-2.972-5.995 1.538-3.448c.4-.7 1.024-.692 1.414 0z" fill="#ffa000" /><path d="M13.445 8.543l2.972 5.995-11.97 11.135z" fill="#f57f17" /><path d="M23.123 7.003c.572-.55 1.164-.362 1.315.417l3.116 18.105-10.328 6.2c-.36.2-1.32.286-1.32.286s-.874-.104-1.207-.3L4.447 25.673z" fill="#ffca28" /><path d="M13.445 8.543l-8.997 17.13L8.455.638c.148-.78.592-.855.988-.167z" fill="#ffa000" /></svg>
+
                   </div>
                 </FramerShell>
               </div>
@@ -109,8 +110,9 @@ export default function Home() {
               <div className="absolute top-[25rem] right-72">
                 <FramerShell direction="left">
                   <div className="flex items-center gap-2 hover:scale-110 duration-200">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="64" viewBox="0 0 32 32" width="64"><path d="M19.62 11.558l-3.203 2.98-2.972-5.995 1.538-3.448c.4-.7 1.024-.692 1.414 0z" fill="#ffa000" /><path d="M13.445 8.543l2.972 5.995-11.97 11.135z" fill="#f57f17" /><path d="M23.123 7.003c.572-.55 1.164-.362 1.315.417l3.116 18.105-10.328 6.2c-.36.2-1.32.286-1.32.286s-.874-.104-1.207-.3L4.447 25.673z" fill="#ffca28" /><path d="M13.445 8.543l-8.997 17.13L8.455.638c.148-.78.592-.855.988-.167z" fill="#ffa000" /></svg>
                     <h1 className="text-3xl font-semibold">Firebase</h1>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="64" viewBox="0 0 32 32" width="64"><path d="M19.62 11.558l-3.203 2.98-2.972-5.995 1.538-3.448c.4-.7 1.024-.692 1.414 0z" fill="#ffa000" /><path d="M13.445 8.543l2.972 5.995-11.97 11.135z" fill="#f57f17" /><path d="M23.123 7.003c.572-.55 1.164-.362 1.315.417l3.116 18.105-10.328 6.2c-.36.2-1.32.286-1.32.286s-.874-.104-1.207-.3L4.447 25.673z" fill="#ffca28" /><path d="M13.445 8.543l-8.997 17.13L8.455.638c.148-.78.592-.855.988-.167z" fill="#ffa000" /></svg>
+
                   </div>
                 </FramerShell>
               </div>
@@ -126,8 +128,9 @@ export default function Home() {
               <div className="absolute top-[35rem] right-56">
                 <FramerShell direction="left">
                   <div className="flex items-center gap-2 hover:scale-110 duration-200">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="64" viewBox="0 0 32 32" width="64"><path d="M19.62 11.558l-3.203 2.98-2.972-5.995 1.538-3.448c.4-.7 1.024-.692 1.414 0z" fill="#ffa000" /><path d="M13.445 8.543l2.972 5.995-11.97 11.135z" fill="#f57f17" /><path d="M23.123 7.003c.572-.55 1.164-.362 1.315.417l3.116 18.105-10.328 6.2c-.36.2-1.32.286-1.32.286s-.874-.104-1.207-.3L4.447 25.673z" fill="#ffca28" /><path d="M13.445 8.543l-8.997 17.13L8.455.638c.148-.78.592-.855.988-.167z" fill="#ffa000" /></svg>
                     <h1 className="text-3xl font-semibold">Firebase</h1>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="64" viewBox="0 0 32 32" width="64"><path d="M19.62 11.558l-3.203 2.98-2.972-5.995 1.538-3.448c.4-.7 1.024-.692 1.414 0z" fill="#ffa000" /><path d="M13.445 8.543l2.972 5.995-11.97 11.135z" fill="#f57f17" /><path d="M23.123 7.003c.572-.55 1.164-.362 1.315.417l3.116 18.105-10.328 6.2c-.36.2-1.32.286-1.32.286s-.874-.104-1.207-.3L4.447 25.673z" fill="#ffca28" /><path d="M13.445 8.543l-8.997 17.13L8.455.638c.148-.78.592-.855.988-.167z" fill="#ffa000" /></svg>
+
                   </div>
                 </FramerShell>
               </div>
